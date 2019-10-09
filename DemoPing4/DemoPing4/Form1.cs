0229d4ef626ac5e2145f3b28c4896f1601c64702
Thanh829 <42971407+Thanh829@users.noreply.github.com>
2019-10-09 17:36:36 +0700

    Add files via upload

---
+++ DemoPing4/DemoPing4/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace DemoPing4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        
+        public DataTable dt = new DataTable();
+        
+        Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add("IP", typeof(string));
+            dt.Columns.Add("RespondFrom", typeof(string));
+            dt.Columns.Add("Type", typeof(string));
+            dt.Columns.Add("Code", typeof(string));
+            dt.Columns.Add("Identifer", typeof(string));
+            dt.Columns.Add("Sequence", typeof(string));
+            dt.Columns.Add("Data", typeof(string));
+
+        }
+        List<string> ListIP=new List<string>(20);
+        private void BtnPing_Click(object sender, EventArgs e)
+        {
+            // Lấy danh sách địa chỉ 
+            
+           
+            for (int i = 0; i<richTextBox2.Lines.Length;i++)
+            {
+                ListIP.Add ( richTextBox2.Lines[i]);
+                DataRow row;
+                row = dt.NewRow();
+                row["IP"] = richTextBox2.Lines[i];
+                dt.Rows.Add(row);
+                
+                
+            }
+            
+            dataGridView1.DataSource = dt;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Thread PingThread = new Thread(ping);
+                PingThread.Start(i);
+            }
+           
+        }
+        public void ping(object No)
+        {
+            byte[] data = new byte[1024];
+            int recv;
+            int stt = Convert.ToInt32(No);
+            string IP = dt.Rows[stt].ItemArray[0].ToString();
+            IPEndPoint iep = null;
+           
+            iep = new IPEndPoint(IPAddress.Parse(IP), 0);
+            EndPoint ep = (EndPoint)iep;
+            ICMP packet = new ICMP();
+            packet.Type = 0x08;
+            packet.Code = 0x00;
+            packet.CheckSum = 0;
+            Buffer.BlockCopy(BitConverter.GetBytes((short)1), 0, packet.Message, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes((short)1), 0, packet.Message, 2, 2);
+            data = Encoding.ASCII.GetBytes("test Packet");
+            Buffer.BlockCopy(data, 0, packet.Message, 4, data.Length);
+            packet.MessageSize = data.Length + 4;
+            int PacketSize = packet.MessageSize + 4;
+            UInt16 checksm = packet.getCheckSum();
+            packet.CheckSum = checksm;
+            host.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+            host.SendTo(packet.getBytes(), PacketSize, SocketFlags.None, iep);
+            try
+            {
+                data = new byte[1024];
+                recv = host.ReceiveFrom(data, ref ep);
+
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("No repsonse from remote host");
+                dataGridView1.Rows[stt].Cells[6].Value = "No repsonse from remote host";
+                return;
+            }
+            ICMP Response = new ICMP(data, recv);
+           
+            int Identifier = BitConverter.ToInt16(Response.Message, 0);
+            int Sequence = BitConverter.ToInt16(Response.Message, 2);
+            
+            string stringData = Encoding.ASCII.GetString(Response.Message, 4, Response.MessageSize - 4);
+           
+            dataGridView1.Rows[stt].Cells[1].Value = ep.ToString();
+            dataGridView1.Rows[stt].Cells[2].Value = Response.Type;
+            dataGridView1.Rows[stt].Cells[3].Value = Response.Code;
+            dataGridView1.Rows[stt].Cells[4].Value = Identifier;
+            dataGridView1.Rows[stt].Cells[5].Value = Sequence;
+            dataGridView1.Rows[stt].Cells[6].Value = stringData;
+
+            host.Close();
+            Thread.Sleep(1000);
+        }
+        
+    }
+}
---
+++ DemoPing4/DemoPing4/ICMP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DemoPing4
+{
+    class ICMP
+    {
+        public byte Type;
+        public byte Code;
+        public UInt16 CheckSum;
+        public int MessageSize;
+        public byte[] Message = new byte[1024];
+        public ICMP()
+        { }
+        public ICMP(byte[] data, int size)
+        {
+            Type = data[20];
+            Code = data[21];
+            CheckSum = BitConverter.ToUInt16(data, 22);
+            MessageSize = size - 24;
+            Buffer.BlockCopy(data, 24, Message, 0, MessageSize);
+        }
+        public byte[] getBytes()
+        {
+            byte[] data = new byte[MessageSize + 9];
+            Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, data, 0, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(Code), 0, data, 1, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(CheckSum), 0, data, 2, 2);
+            Buffer.BlockCopy(Message, 0, data, 4, MessageSize);
+            return data;
+        }
+        public UInt16 getCheckSum()
+        {
+            UInt32 checksm = 0;
+            byte[] data = getBytes();
+            int PacketSize = MessageSize + 8;
+            int index = 0;
+            while (index < PacketSize)
+            {
+                checksm += Convert.ToUInt32(BitConverter.ToUInt16(data, index));
+                index += 2;
+            }
+            checksm = (checksm >> 16) + (checksm & 0xffff);
+            checksm += (checksm >> 16);
+            return (UInt16)(~checksm);
+        }
+    }
+}
